7c6763333d4b1da79d5abdb0578e817b73ede260
Hanna Nguyen <ha.nguyenthidan@gmail.com>
2017-11-28 16:54:26 -0600

    centering table and pie chart

--- server/public/scripts/controllers/budget.controller.js
+++ server/public/scripts/controllers/budget.controller.js
@@ -10,20 +10,7 @@ myApp.controller('BudgetController', function (UserService, $http) {
 
     vm.categoryList = [];
 
-    // Chart
-    //vm.chartData = [];
-    // CHART
-    //vm.myChart = document.getElementById('myChart').getContext('2d');
-
-    // Global Options
-    // Chart.defaults.global.defaultFontFamily = 'Lato';
-    // Chart.defaults.global.defaultFontSize = 9;
-    // Chart.defaults.global.defaultFontColor = '#777';
     
-    // Month Picker
-    // vm.example = {
-    //     value = new Date (2017, 9, 1)
-    // };
 
 
     // UPDATE after-tax income for the month
@@ -32,7 +19,7 @@ myApp.controller('BudgetController', function (UserService, $http) {
         var id = UserService.userObject.id;   // Set id equals to id property within userObject in UserService
         $http.put('/budget/' + id, {income: userIncome}).then( function (response){
             console.log('success', response);
-            //vm.viewBudgets();
+            vm.viewCategory();
         }).catch( function (error) {
             console.log('failure', error);    
         });    
@@ -100,110 +87,120 @@ myApp.controller('BudgetController', function (UserService, $http) {
     
     vm.viewCategory();
 
+    
+
+    // Budgets Chart
+    vm.budgetChart = [];
+
+    vm.myBudgetChart = document.getElementById('myBudgetChart').getContext('2d');
+
+    // Chart: Global Options
+    Chart.defaults.global.defaultFontFamily = 'Lato';
+    Chart.defaults.global.defaultFontSize = 9;
+    Chart.defaults.global.defaultFontColor = '#777';
+
     // GET CHART
-    // vm.requestChart = function () {
-    //     budgetLabel = [];
-    //     budgetAmount = [];
-    //     transactionList = [];
-    //     transactionAmount = [];
-    //     $http.get('/budget').then(function (response){
-    //         //vm.budgetLabel =[];
-    //         vm.chartData = response.data;
-    //         for ( var i = 0; i < vm.chartData.length; i++ ) {
-    //             budgetAmount.push(vm.chartData[i].amount);
-    //             budgetLabel.push (vm.chartData[i].category_name);
-    //             //console.log ("aaa", budgetLabel);
-    //         }
-    //         $http.get('/transaction').then(function (response) {
-    //             console.log('success importing transactions');
-    //             transactionList = response.data;
-    //             console.log("Long Nguyen", transactionList);
-    //             for (i=0; i< budgetLabel.length; i++){
-    //                 sum = 0;
-    //                 for (var j=0; j<transactionList.length; j++) {
-    //                     if (budgetLabel[i] == transactionList[j].category_name) {
-    //                         sum += parseInt(transactionList[j].amount);
-    //                     }
-    //                 }
-    //                 transactionAmount.push(sum);
-    //             }
-    //             vm.budgetChart = new Chart(myChart, {
-    //                 type: 'line', // bar,pie, line, horizontalBar
-    //                 data: {
-    //                     labels: budgetLabel,
-    //                     datasets: [{
-    //                         label: 'Budgets',
-    //                         fill: false,
-    //                         lineTension: 0.7,
-    //                         data: budgetAmount,
-    //                         // backgroundColor: 'green',
-    //                         backgroundColor: [
-    //                             'rgba(255, 99, 132, 0.6)',
-    //                             'rgba(54, 162, 235, 0.6)',
-    //                             'rgba(255, 206, 86, 0.6)',
-    //                             // 'rgba(153, 102, 255, 0.6)',
-    //                             // 'rgba(255, 159, 64, 0.6)',
-    //                             // 'rgba(255, 99, 132, 0.6)',
-    //                             'rgba(75, 192, 192, 0.6)',
-    //                         ],
-    //                         borderWidth: 1,
-    //                         borderColor: '#777',
-    //                         hoverBorderWidth: 3,
-    //                         hoverBorderColor: '#000'
-    //                     },
-    //                     {
-    //                         label: 'Category Amount',
-    //                         fill: true,
-    //                         lineTension: 0.1,
-    //                         data: transactionAmount,
-    //                         // backgroundColor: 'green',
-    //                         backgroundColor: [
-    //                             'rgba(255, 99, 132, 0.6)',
-    //                             'rgba(54, 162, 235, 0.6)',
-    //                             'rgba(255, 206, 86, 0.6)',
-    //                             // 'rgba(153, 102, 255, 0.6)',
-    //                             // 'rgba(255, 159, 64, 0.6)',
-    //                             // 'rgba(255, 99, 132, 0.6)',
-    //                             'rgba(75, 192, 192, 0.6)',
-    //                         ],
-    //                         borderWidth: 1,
-    //                         borderColor: '#777',
-    //                         hoverBorderWidth: 3,
-    //                         hoverBorderColor: '#000'
-    //                     }
-    //                     ]
-    //                 },
-    //                 options: {
-    //                     title: {
-    //                         display: true,
-    //                         text: 'Budgets of the month',
-    //                         fontSize: 25
-    //                     },
-    //                     legend: {
-    //                         position: 'right',
-    //                         labels: {
-    //                             fontColor: '#000'
-    //                         }
-    //                     },
-    //                     layout: {
-    //                         padding: {
-    //                             left: 50,
-    //                             right: 0,
-    //                             bottom: 0,
-    //                             top: 0
-    //                         }
-    //                     },
-    //                     tooltops: {
-    //                         enabled: true
-    //                     }
-    //                 }
-    //             })
-    //         }).catch(function (error) {
-    //             console.log('failure', error);
-    //         });
-    //     }).catch(function (error){
-    //         console.log('failure', error);  
-    //     });
-    //     //console.log("Display: ", vm.budgetAmount);
-    // }
+    vm.requestBudgetChart = function () {
+        budgetLabel = [];
+        budgetAmount = [];
+        // transactionList = [];  // Transaction Names
+        // transactionAmount = [];
+        $http.get('/budget').then(function (response) {
+            vm.budgetChart = response.data;
+            console.log('chartData', vm.budgetChart);
+            for (var i = 0; i < vm.budgetChart.length; i++) {
+                budgetAmount.push(vm.budgetChart[i].amount);
+                budgetLabel.push(vm.budgetChart[i].category_name);
+                //console.log ("budgetLabel", budgetLabel);
+            }
+            // $http.get('/transaction').then(function (response) {
+            //     console.log('success importing transactions');
+            //     transactionList = response.data;
+            //     console.log("transactionList", transactionList);
+            //     for (i = 0; i < budgetLabel.length; i++) {
+            //         sum = 0;
+            //         for (var j = 0; j < transactionList.length; j++) {
+            //             if (budgetLabel[i] == transactionList[j].category_name) {
+            //                 sum += parseInt(transactionList[j].amount);
+            //             }
+            //         }
+            //         transactionAmount.push(sum);
+            //     }
+            vm.budgetChart = new Chart(myBudgetChart, {
+                    type: 'pie', // bar,pie, line, horizontalBar
+                    data: {
+                        labels: budgetLabel,
+                        datasets: [{
+                            label: 'Budgets',
+                            fill: false,
+                            lineTension: 0.7,
+                            data: budgetAmount,                    
+                            backgroundColor: 'green',
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.6)',
+                                'rgba(54, 162, 235, 0.6)',
+                                'rgba(255, 206, 86, 0.6)',
+                                'rgba(153, 102, 255, 0.6)',
+                                'rgba(255, 159, 64, 0.6)',
+                                'rgba(255, 99, 132, 0.6)'],
+                            borderWidth: 1,
+                            borderColor: '#777',
+                            hoverBorderWidth: 3,
+                            hoverBorderColor: '#000'
+                        }]
+                    },
+                    //     {
+                    //         label: 'Category Amount',
+                    //         fill: true,
+                    //         lineTension: 0.1,
+                    //         data: transactionAmount,
+                    //         // backgroundColor: 'green',
+                    //         backgroundColor: [
+                    //             'rgba(255, 99, 132, 0.6)',
+                    //             'rgba(54, 162, 235, 0.6)',
+                    //             'rgba(255, 206, 86, 0.6)',
+                    //             // 'rgba(153, 102, 255, 0.6)',
+                    //             // 'rgba(255, 159, 64, 0.6)',
+                    //             // 'rgba(255, 99, 132, 0.6)',
+                    //             'rgba(75, 192, 192, 0.6)',
+                    //         ],
+                    //         borderWidth: 1,
+                    //         borderColor: '#777',
+                    //         hoverBorderWidth: 3,
+                    //         hoverBorderColor: '#000'
+                    //     }
+                    //     ]
+
+                    options: {
+                        title: {
+                            display: true,
+                            text: 'Budgets of the month',
+                            fontSize: 25
+                        },
+                        legend: {
+                            position: 'right',
+                            labels: {
+                                fontColor: '#000'
+                            }
+                        },
+                        layout: {
+                            padding: {
+                                left: 50,
+                                right: 0,
+                                bottom: 0,
+                                top: 0
+                            }
+                        },
+                        tooltops: {
+                            enabled: true
+                        }
+                    }
+                })
+                    
+        }).catch(function (error) {
+            console.log('failure', error);
+        });
+        //console.log("Display: ", vm.budgetAmount);
+    }
+
 });
